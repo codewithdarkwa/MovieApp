295d3db6b83296f1c19e7a803c25166a8cfcd3bd
Darkwa <jnrdrkw1@gmail.com>
2022-04-26 09:37:38 +0000

    making the movie card clickable

--- MovieCard.js
+++ MovieCard.js
@@ -1,18 +1,20 @@
-import { View, Image } from "react-native";
+import { View, Image, TouchableOpacity } from "react-native";
 import React from "react";
 
 export default function MovieCard({ movie }) {
   return (
     <View style={{ justifyContent: "center", alignItems: "center" }}>
-      <Image
-        style={{
-          width: 150,
-          height: 200,
-          margin: 10,
-          borderRadius: 5,
-        }}
-        source={{ uri: movie.Poster }}
-      />
+      <TouchableOpacity>
+        <Image
+          style={{
+            width: 150,
+            height: 200,
+            margin: 10,
+            borderRadius: 10,
+          }}
+          source={{ uri: movie.Poster }}
+        />
+      </TouchableOpacity>
     </View>
   );
 }
